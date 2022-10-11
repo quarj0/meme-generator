6054238f203c2be5474f0c7a39a979ca4121e770
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-10-11 18:41:53 +0000

    finished but not done

--- src/components/meme.js
+++ src/components/meme.js
@@ -18,19 +18,45 @@ const url = memesArray[randomNumber].url
         randomImage : url
       }))
     }
+
+    function changehandler(event){
+      const{name, value} = event.target
+      setmeme(prevMeme => ({
+        ...prevMeme,
+       [name]: value
+      }))
+    }
+
   return (
     <main>
         <div className="form">
-        <input className='form-input' 
-        type="text" />
-        <input className='form-input' 
-        type="text" />
+        <input 
+        type="text"
+        placeholder='Top text'
+        className='form-input'
+         name='topText'
+        value={meme.topText} 
+        onChange={changehandler} 
+        />
+        <input 
+        placeholder='Bottom text'
+        type="text" 
+        className='form-input'
+         name='bottomText'
+        value={meme.bottomText}
+        onChange={changehandler}
+        />
         <button onClick={getMeme} 
         className='form-btn' 
         >Get new meme image 🖼️</button>
       </div>
+      <div className='meme'>
+      <h2 className='meme-text-top'>{meme.topText}</h2>
+       <h2 className='meme-text-down'>{meme.bottomText}</h2>
       <img src={meme.randomImage}
        className='meme-image' alt='meme' />
+       
+       </div>
     </main>
   )
 }
