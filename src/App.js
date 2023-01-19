2fbf779f0d614c48b14a92d5dd6ae7aeb546d079
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-01-19 10:44:41 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Header from './components/header'
-import Meme from './components/meme'
+import Header from "./components/header";
+import Meme from "./components/meme";
+import React from 'react';
 // import Practice from './components/practice';
 function App() {
   return (
-  <div>
-    <Header />
     <div>
-      <Meme />
+      <Header />
+      <div>
+        <Meme />
+      </div>
+      <hr />
     </div>
-    <hr />
-  </div>
- 
   );
 }
 
